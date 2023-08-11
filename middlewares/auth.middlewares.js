7bd3bdebc971063b0a4ae2f50134be1e4b97fe97
Antonio Navas <antonioluis_7_@hotmail.com>
2023-08-11 18:16:53 +0200

    "admin/user role"

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,3 +1,4 @@
+//funcion que comprueba si esta log in
 function isLoggedIn(req, res, next) {
   if (req.session.user === undefined) {
     //usuario no está logeado
@@ -8,6 +9,7 @@ function isLoggedIn(req, res, next) {
   }
 }
 
+//funcion que comprueba si el rol del user es admin
 function isAdmin(req, res, next) {
   if (req.session.user.role === "admin") {
     next(); // continua
@@ -18,16 +20,22 @@ function isAdmin(req, res, next) {
 
 function updateLocals(req, res, next) {
   if (req.session.user === undefined) {
+    //variable que indica que el user no esta logeado
     res.locals.isUserActive = false;
   } else {
+    //variable que indica que el user si esta logeado
     res.locals.isUserActive = true;
+    if (req.session.user.role === "admin"){
+      res.locals.isAdminActive = true;
+    } else {
+      res.locals.isAdminActive = false;
+    }
   }
-
   next();
 }
 
 module.exports = {
   isLoggedIn: isLoggedIn,
   isAdmin: isAdmin,
-  updateLocals: updateLocals
+  updateLocals: updateLocals,
 };
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//  const {updatelocal} = require("../middlewares/auth.middlewares")
-//  router.use(updatelocal)
+//funciona que actualiza las variables en las diferentes llamadas
+const { updateLocals } = require("../middlewares/auth.middlewares");
+router.use(updateLocals);
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-const authRouter = require("./auth.routes.js")
-router.use("/auth", authRouter)
+const authRouter = require("./auth.routes.js");
+router.use("/auth", authRouter);
 
-const userRouter = require("./user.routes.js")
-router.use("/user", userRouter)
+const userRouter = require("./user.routes.js");
+router.use("/user", userRouter);
 
 module.exports = router;
